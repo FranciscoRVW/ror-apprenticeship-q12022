1a2728d92723e47f5a870fec9becb6c2c4accbf8
franciscorvw <francisco.rv@wizeline.com>
2022-01-27 09:37:02 -0600

    Create scaffold for Pokemon and Trainer

---
+++ capstone_project/app/controllers/pokemons_controller_old.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class PokemonsController < ApplicationController
+  def index; end
+  def show; end
+  def show_trainers; end
+  def show_types; end
+  def show_catched; end
+end
--- capstone_project/config/routes.rb
+++ capstone_project/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :trainers
   root 'pokemons#index'
 
   resources :pokemons
